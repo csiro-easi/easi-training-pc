47a0e03a720ded6b14178e92a8e47dacf302bd8a
Rob Woodcock <Robert.Woodcock@csiro.au>
2018-11-21 17:55:53 +1100

    add default "secretpassword" token to jupyter and tweak python packages

--- easi-pc-py36/start-notebook.sh
+++ easi-pc-py36/start-notebook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # Install ODC dev environment - only once
-if [ ! $(pip3 freeze 2> /dev/null | grep coveralls==) ]; then
+if [ ! $(pip3 freeze 2> /dev/null | grep datacube==) ]; then
     # Now use the setup.py file to identify dependencies
     cd $HOME/odc
     export CPLUS_INCLUDE_PATH=/usr/include/gdal
@@ -14,7 +14,7 @@ if [ ! $(pip3 freeze 2> /dev/null | grep coveralls==) ]; then
     # # Fix an issue with the pandas pinned version in requirements-test.txt not being appropriate (ImportError: cannot import name 'AbstractMethodError').
     # # TODO: This should be fixed in datacube-core on the CSIRO branch or fork as will be determined in future.
     # Add missing python packages that are required for things like data inges
-    pip3 install pandas shapely
+    pip3 install pandas
 fi
 # Start Jupyter Notebook
 cd $HOME/work
@@ -26,6 +26,6 @@ else
   if [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
     . /usr/local/bin/start.sh ~/.local/bin/jupyter lab $*
   else
-    . /usr/local/bin/start.sh ~/.local/bin/jupyter notebook $*
+    . /usr/local/bin/start.sh ~/.local/bin/jupyter notebook --NotebookApp.token='secretpassword' $*
   fi
 fi
